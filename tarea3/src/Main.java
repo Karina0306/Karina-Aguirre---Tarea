937fd2f1bef411ea32afe9d13ba247c6cb30fc34
Karina0306 <106938364+Karina0306@users.noreply.github.com>
2022-07-27 19:07:40 -0400

    Add files via upload

---
+++ tarea3/src/Main.java
@@ -0,0 +1,10 @@
+import Arreglos.Arrangement;
+
+public class Main {
+    public static void main(String[] args) {
+        Arrangement arrangement = new Arrangement();
+        arrangement.arrayIntroduceNumbers();
+        arrangement.printArray();
+        arrangement.mostFrequent();
+    }
+}
