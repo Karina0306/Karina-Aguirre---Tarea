6ea45e0dfef0722ab1b17925f23945cbac0e37ab
Karina0306 <106938364+Karina0306@users.noreply.github.com>
2022-07-29 00:21:52 -0400

    Add files via upload

---
+++ Tarea4/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr1 = {2, 1, 2, 2, 6, 5, 0, 2, 0, 5, 5, 7, 7, 4, 3, 3, 9};
+        Solution solution = new Solution();
+        System.out.println(solution.sumGroups(arr1));
+    }
+}
